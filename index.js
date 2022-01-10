8bc309e7a5ebd834e976a0e87e3d1033bc7e55c7
sooof <sooof@me.com>
2022-01-10 15:36:54 -0800

    use express

--- index.js
+++ index.js
@@ -1,14 +1,21 @@
 // import the server and start it!
-const http = require("http"); // built in node.js module to handle http traffic
+// const http = require("http"); // built in node.js module to handle http traffic
+const express = require('express');
 
 const port = 3000; // a port we'll use to watch for traffic
 
-const server = http.createServer((req, res) => {
-    // creates our server
-    res.statusCode = 200; // http status code returned to the client
-    res.setHeader("Content-Type", "text/plain"); // inform the client that we'll be returning text
-    res.end("Hello World from Node\n"); // end the request and send a response with the specified message
-});
+const server = express();
+
+// const server = http.createServer((req, res) => {
+//     // creates our server
+//     res.statusCode = 200; // http status code returned to the client
+//     res.setHeader("Content-Type", "text/plain"); // inform the client that we'll be returning text
+//     res.end("Hello World from Node\n"); // end the request and send a response with the specified message
+// });
+
+server.get('/', (req, res)=> {
+  res.send('Hello world from Express!')
+})
 
 server.listen(port, () => {
     // start watching for connections on the port specified
@@ -19,3 +26,20 @@ server.listen(port, () => {
 //  node index.js 
 //  npm init -y
 //  yarn add express
+//  npx eslint --init
+//  npx gitignore node
+//  npm i -D nodemon 
+//  npm install express nanoid
+
+// curl localhost:9000/hello_world -v
+
+
+
+/** 
+ * 12> del git cached
+```
+git rm -r --cached .
+git add .
+git commit -m 'update .gitignore'
+```
+*/
