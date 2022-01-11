91d2c2f2aec2cb6279644bd7d72efa524c005950
sooof <sooof@me.com>
2022-01-10 18:03:14 -0800

    web server hello world work

--- index.js
+++ index.js
@@ -1,25 +1,32 @@
-// // import the server and start it!
-// const express = require('express') // commonjs
-// // import express from 'express'
+// import the server and start it!
+const express = require('express') // commonjs
+// import express from 'express'
 
-// const server = express()
+const server = express()
 
-// server.use(express.json()) // parse json from requests
+server.use(express.json()) // parse json from requests
 
-// // api is made of endpoints such as
-// // http://cats.com:9000/hello_world
-// server.get('/hello_world', (req, res) => {
-//   res.json('hello world!')
-// })
+// api is made of endpoints such as
+// http://cats.com:9000/hello_world
+server.get('/hello_world', (req, res) => {
+  res.json('hello world!')
+})
 
-// server.listen(9000, () => {
-//   console.log('listening on port 9000')
-// })
+server.listen(9000, () => {
+  console.log('listening on port 9000')
+})
 
-console.log('Web 49 test')
+// console.log('Web 49 test')
 
 //  node index.js 
 //  npx gitignore node
 //  npm init -y  // creat package.json
 //  npx eslint --init
+//  npm i  nodemon 
 //  npm install express nanoid
+
+
+//  npm i -D nodemon 
+
+
+// curl localhost:9000/hello_world -v
